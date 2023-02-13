db02b4842bc42a6450b391c255ea314f503ed16a
Eli Slothower <eslothow@andrew.cmu.edu>
2023-02-13 18:54:07 -0500

    Fixed linter error

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,7 +33,7 @@ module.exports = function (Topics) {
             lastposttime: 0,
             postcount: 0,
             viewcount: 0,
-            isAnonymous: data.isAnonymous
+            isAnonymous: data.isAnonymous,
         };
 
         if (Array.isArray(data.tags) && data.tags.length) {
