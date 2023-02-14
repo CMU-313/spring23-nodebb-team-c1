5d85243aee53ce4ac365a67e82481b150b10a9c8
cindycyx <yanxinc@andrew.cmu.edu>
2023-02-13 19:15:51 -0500

    fixed linter & tests

--- src/topics/create.js
+++ src/topics/create.js
@@ -36,7 +36,7 @@ module.exports = function (Topics) {
             lastposttime: 0,
             postcount: 0,
             viewcount: 0,
-            isPrivate: data.isPrivate
+            isPrivate: data.isPrivate,
         };
 
         if (Array.isArray(data.tags) && data.tags.length) {
